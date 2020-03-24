45d98d1cf49f6d3f6f6bc0ce922c1f123bb0204a
Emin <eminokic@iastate.edu>
2020-03-24 16:19:29 -0500

    Hello World Application

---
+++ Exercises/hello_world/src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello traveler, what is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
