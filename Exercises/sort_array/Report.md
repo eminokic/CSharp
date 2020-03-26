321b69b26bb6719ff88cf684b7cfc32e4fb7b486
Emin <eminokic@iastate.edu>
2020-03-26 11:19:20 -0500

    Sort Array Exercise

---
+++ Exercises/sort_array/Report.md
@@ -0,0 +1,24 @@
+# C# Sorting 
+
+Sorting Exercise
+
+## Purpose
+
+Understand how to sort in C#, and what built in functions are offerred. I learned that C# provides great tools for creating sorted, and descending arrays. For the future, I will apply other sorting algorithms into C# code to further strengthen this skillset.
+
+```C#
+"Original Array: 2, 1, 5, 3, 4"
+"Sorted Array: 1, 2, 3, 4, 5"
+"Descending Array: 5, 4, 3, 2, 1"
+```
+
+## Concepts
+
+```C#
+using namespace System
+
+Random random = new Random(); //Creates a random object
+random.Next(min, max); //Generates the next random int within min and max bounds
+Array.Sort(array); //C# built in function to sort array in ascending order
+Array.Reverse(array); // C# built in function to reverse arrays
+```
---
+++ Exercises/sort_array/src/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize an array and random object
+            var randomArray = new int[9];
+            Random rand = new Random();
+
+            //Insert pseudo random numbers into each index of the array
+            for(int i = 0; i < randomArray.Length; i++) {
+                randomArray[i] = rand.Next(0,99);
+            }
+
+            string originalArray = string.Join(",", randomArray);
+            Console.WriteLine("Original Array: " + originalArray);
+
+            //Sort in Ascending Order Using C#'s Sort Method  
+            Array.Sort(randomArray);
+            string sortedArr = string.Join(",", randomArray);
+            Console.WriteLine("Sorted Array: " + sortedArr);
+
+            //Reverse array to recieve it in descending order after sort
+            Array.Reverse(randomArray);
+            string reverseArr = string.Join(",", randomArray);
+            Console.WriteLine("Descending Array: " + reverseArr);
+        }
+    }
+}
